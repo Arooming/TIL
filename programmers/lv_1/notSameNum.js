c383962f2cccd5104c9994e6a0743c862dea0dd2
seoAreum <1971236@hansung.ac.kr>
2023-08-11 01:12:21 +0900

    feat: 08/11 TIL

---
+++ programmers/lv_1/notSameNum.js
@@ -0,0 +1,16 @@
+// 같은 숫자는 싫어
+function solution(arr) {
+  var answer = [];
+
+  answer.push(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[i - 1]) {
+      answer.push(arr[i]);
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution([1, 1, 3, 3, 0, 1, 1]));
+console.log(solution([4, 4, 4, 3, 3]));
