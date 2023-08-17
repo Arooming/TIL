586c8207bf9c71648ccdcda3f7e68ceaff58cc74
seoAreum <1971236@hansung.ac.kr>
2023-08-18 04:15:30 +0900

    feat: 08/17 TIL

---
+++ programmers/lv_1/notCompletedPart.js
@@ -0,0 +1,24 @@
+// 완주하지 못한 선수
+// 해시 공부 후, 해시를 활용한 코드로 다시 풀어보기
+
+function solution(participant, completion) {
+  participant.sort();
+  completion.sort();
+
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      return participant[i];
+    }
+  }
+}
+
+console.log(solution(["leo", "kiki", "eden"], ["eden", "kiki"]));
+console.log(
+  solution(
+    ["marina", "josipa", "nikola", "vinko", "filipa"],
+    ["josipa", "filipa", "marina", "nikola"]
+  )
+);
+console.log(
+  solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
+);
