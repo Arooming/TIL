c9cb0d02bd1dd59e2509b05cccfbde7c4c79f779
seoAreum <1971236@hansung.ac.kr>
2023-08-19 15:18:23 +0900

    review: 08/19 TIL

--- programmers/review.js
+++ programmers/review.js
@@ -1,14 +1,31 @@
-function solution(participant, completion) {
-  participant.sort();
-  completion.sort();
+function solution(sizes) {
+  let width = [];
+  let height = [];
 
-  for (let i = 0; i < participant.length; i++) {
-    if (participant[i] !== completion[i]) {
-      return participant[i];
-    }
+  for (let i = 0; i < sizes.length; i++) {
+    sizes[i][0] > sizes[i][1]
+      ? (sizes[i] = [sizes[i][0], sizes[i][1]])
+      : (sizes[i] = [sizes[i][1], sizes[i][0]]);
+
+    const max = sizes[i][0];
+    const min = sizes[i][1];
+
+    width.push(max);
+    height.push(min);
   }
+
+  width.sort((a, b) => b - a);
+  height.sort((a, b) => b - a);
+
+  return width[0] * height[0];
 }
 
 console.log(
-  solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
+  solution([
+    [10, 7],
+    [12, 3],
+    [8, 15],
+    [14, 7],
+    [5, 15],
+  ])
 );
