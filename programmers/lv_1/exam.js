b902594e6f286497aa7b96acf48b9e31aebd07cb
seoAreum <1971236@hansung.ac.kr>
2023-08-20 22:23:45 +0900

    Update exam.js
    가장 성능 좋은 코드로 수정

--- programmers/lv_1/exam.js
+++ programmers/lv_1/exam.js
@@ -1,41 +1,29 @@
 // 모의고사
 function solution(answers) {
-  var answer = [];
+  const answerA = [1, 2, 3, 4, 5];
+  const answerB = [2, 1, 2, 3, 2, 4, 2, 5];
+  const answerC = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-  let a = [1, 2, 3, 4, 5];
-  let b = [2, 1, 2, 3, 2, 4, 2, 5];
-  let c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  const result = [0, 0, 0];
 
-  let countA = 0;
-  let countB = 0;
-  let countC = 0;
+  let finalAnswer = [];
 
-  for (let i = 0; i < answers.length; i++) {
-    if (answers[i] === a[i % a.length]) {
-      countA += 1;
-    }
-    if (answers[i] === b[i % b.length]) {
-      countB += 1;
-    }
-    if (answers[i] === c[i % c.length]) {
-      countC += 1;
-    }
-  }
+  answers.map((answer, i) =>
+    answer === answerA[i % answerA.length] ? result[0]++ : result[0]
+  );
+  answers.map((answer, i) =>
+    answer === answerB[i % answerB.length] ? result[1]++ : result[1]
+  );
+  answers.map((answer, i) =>
+    answer === answerC[i % answerC.length] ? result[2]++ : result[2]
+  );
 
-  let max = Math.max(countA, countB, countC);
+  let max = Math.max(result[0], result[1], result[2]);
 
-  if (countA === max) {
-    answer.push(1);
-  }
+  max === result[0] && finalAnswer.push(1);
+  max === result[1] && finalAnswer.push(2);
+  max === result[2] && finalAnswer.push(3);
 
-  if (countB === max) {
-    answer.push(2);
-  }
-  if (countC === max) {
-    answer.push(3);
-  }
-
-  return answer;
+  return finalAnswer;
 }
-
 console.log(solution([1, 3, 2, 4, 2]));
