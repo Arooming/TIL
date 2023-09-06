cd88665f3388b911e74f0bafb75547686817492e
seoAreum <1971236@hansung.ac.kr>
2023-09-06 17:18:18 +0900

    Update exam.js

--- programmers/lv_1/exam.js
+++ programmers/lv_1/exam.js
@@ -1,5 +1,35 @@
 // 모의고사
 function solution(answers) {
+  var counter = [0, 0, 0];
+  let a = [1, 2, 3, 4, 5];
+  let b = [2, 1, 2, 3, 2, 4, 2, 5];
+  let c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  let answer = [];
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === a[i % a.length]) {
+      counter[0]++;
+    }
+    if (answers[i] === b[i % b.length]) {
+      counter[1]++;
+    }
+    if (answers[i] === c[i % c.length]) {
+      counter[2]++;
+    }
+  }
+
+  for (let j = 0; j < counter.length; j++) {
+    if (Math.max(counter[0], counter[1], counter[2]) === counter[j]) {
+      answer.push(j + 1);
+    }
+  }
+
+  return answer;
+}
+
+/*
+function solution(answers) {
   const answerA = [1, 2, 3, 4, 5];
   const answerB = [2, 1, 2, 3, 2, 4, 2, 5];
   const answerC = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
@@ -26,4 +56,7 @@ function solution(answers) {
 
   return finalAnswer;
 }
+*/
+
+console.log(solution([1, 2, 3, 4, 5]));
 console.log(solution([1, 3, 2, 4, 2]));
