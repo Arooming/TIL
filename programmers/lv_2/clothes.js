e6a9aaeb292132f5336a1529a3a9a8b132bc6f82
seoAreum <1971236@hansung.ac.kr>
2023-09-18 01:45:54 +0900

    Update clothes.js

--- programmers/lv_2/clothes.js
+++ programmers/lv_2/clothes.js
@@ -1,14 +1,23 @@
+// 의상
 function solution(clothes) {
-  let types = {};
-  let result = 1;
+  let answer = 1;
+  let obj = {};
 
-  clothes.forEach((v) => (types[v[1]] = (types[v[1]] || 0) + 1));
-
-  for (const key in types) {
-    result *= types[key] + 1;
+  for (let i = 0; i < clothes.length; i++) {
+    // obj[key] = value;
+    // 카테고리에 해당하는 의상이 없는 경우, 1로 대체
+    // 카테고리에 해당하는 의상이 있는 경우, 의상의 개수 1 증가
+    obj[clothes[i][1]] = (obj[clothes[i][1]] || 1) + 1;
   }
 
-  return result - 1;
+  for (let key in obj) {
+    // key는 obj의 key 값
+    // obj[key]는 obj의 value 값
+    answer *= obj[key];
+  }
+
+  // 의상을 모두 입지 않는 경우는 제외
+  return answer - 1;
 }
 
 console.log(
