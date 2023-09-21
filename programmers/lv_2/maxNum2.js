53a1abd049846f6d3aa38d33c78fa1c74d8692cb
seoAreum <1971236@hansung.ac.kr>
2023-09-21 23:07:34 +0900

    feat: 09/21 TIL

---
+++ programmers/lv_2/maxNum2.js
@@ -0,0 +1,17 @@
+// 큰 수 만들기
+function solution(number, k) {
+    const arr = [];
+    for (let i = 0; i < number.length; i++) {
+      while (arr.length > 0 && arr[arr.length - 1] < number[i] && k > 0) {
+        k--;
+        arr.pop();
+      }
+      arr.push(number[i]);
+    }
+    arr.splice(number.length - k, k);
+    return arr.join("");
+  }
+
+console.log(solution("1924", 2));
+console.log(solution("1231234", 3));
+console.log(solution("4177252841", 4));
