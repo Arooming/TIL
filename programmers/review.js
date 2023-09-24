38cf960cc1635b79fe3fa6dd8b160e2ee80e0b96
seoAreum <1971236@hansung.ac.kr>
2023-09-25 04:09:39 +0900

    review: 09/24 TIL

--- programmers/review.js
+++ programmers/review.js
@@ -1,46 +1,32 @@
-// 소수 찾기
-const getPermutation = (arr, selectNumber) => {
-  const results = [];
-  if (selectNumber === 1) return arr.map((v) => [v]);
-  else {
-    arr.forEach((fixed, index, origin) => {
-      const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
-      const permutations = getPermutation(rest, selectNumber - 1);
-      const attached = permutations.map((permutation) => [
-        fixed,
-        ...permutation,
-      ]);
-      results.push(...attached);
-    });
-  }
+// 조이스틱
+function solution(name) {
+  let answer = 0;
+  const min = 65;
+  const max = 90 + 1;
 
-  return results;
-};
+  const findChar = (find) => {
+    const findCode = find.charCodeAt();
+    return Math.min(findCode - min, max - findCode);
+  };
 
-const checkPrimeNumber = (number) => {
-  if (number < 2) return false;
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    const remainder = number % i;
-    if (remainder === 0) return false;
-  }
-  return true;
-};
+  const chars = name.split("").map((c) => findChar(c));
+  let minMove = chars.length - 1;
 
-function solution(numbers) {
-  const answer = new Set();
+  chars.forEach((char, idx) => {
+    answer += char;
 
-  for (let i = 1; i <= numbers.length; i++) {
-    const permutation = [...getPermutation([...numbers], i)];
-    const primeNumbers = permutation.filter((arr) => {
-      const number = +arr.join("");
-      const isPrimeNumber = checkPrimeNumber(number);
-      return isPrimeNumber;
-    });
+    let cursor = idx + 1;
+    while (cursor < chars.length && chars[cursor] === 0) cursor++;
 
-    primeNumbers.forEach((arr) => {
-      answer.add(+arr.join(""));
-    });
-  }
+    minMove = Math.min(
+      minMove,
+      idx * 2 + chars.length - cursor,
+      idx + 2 * (chars.length - cursor)
+    );
+  });
 
-  return answer.size;
+  return answer + minMove;
 }
+
+console.log(solution("JEROEN"));
+console.log(solution("JAN"));
