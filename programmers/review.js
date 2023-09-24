261e05e0ebf4a6396ad5a002cdc6df2389adfda0
seoAreum <1971236@hansung.ac.kr>
2023-09-25 04:11:31 +0900

    review: 09/24 TIL

--- programmers/review.js
+++ programmers/review.js
@@ -1,32 +1,17 @@
-// 조이스틱
-function solution(name) {
-  let answer = 0;
-  const min = 65;
-  const max = 90 + 1;
-
-  const findChar = (find) => {
-    const findCode = find.charCodeAt();
-    return Math.min(findCode - min, max - findCode);
-  };
-
-  const chars = name.split("").map((c) => findChar(c));
-  let minMove = chars.length - 1;
-
-  chars.forEach((char, idx) => {
-    answer += char;
-
-    let cursor = idx + 1;
-    while (cursor < chars.length && chars[cursor] === 0) cursor++;
-
-    minMove = Math.min(
-      minMove,
-      idx * 2 + chars.length - cursor,
-      idx + 2 * (chars.length - cursor)
-    );
-  });
-
-  return answer + minMove;
+// 큰 수 만들기
+function solution(number, k) {
+  const arr = [];
+  for (let i = 0; i < number.length; i++) {
+    while (arr.length > 0 && arr[arr.length - 1] < number[i] && k > 0) {
+      k--;
+      arr.pop();
+    }
+    arr.push(number[i]);
+  }
+  arr.splice(number.length - k, k);
+  return arr.join("");
 }
 
-console.log(solution("JEROEN"));
-console.log(solution("JAN"));
+console.log(solution("1924", 2));
+console.log(solution("1231234", 3));
+console.log(solution("4177252841", 4));
