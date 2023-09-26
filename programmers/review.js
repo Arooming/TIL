3c057230452ccbb969ee4f22b054d802fe609165
seoAreum <1971236@hansung.ac.kr>
2023-09-26 11:49:39 +0900

    review: 09/26 TIL

--- programmers/review.js
+++ programmers/review.js
@@ -1,17 +1,34 @@
-// 큰 수 만들기
-function solution(number, k) {
-  const arr = [];
-  for (let i = 0; i < number.length; i++) {
-    while (arr.length > 0 && arr[arr.length - 1] < number[i] && k > 0) {
-      k--;
-      arr.pop();
-    }
-    arr.push(number[i]);
+// 의상
+function solution(clothes) {
+  var answer = 1;
+  let obj = {};
+
+  for (let i = 0; i < clothes.length; i++) {
+    // 카테고리에 해당하는 의상이 있는 경우 +1
+    // 카테고리에 해당하는 의상이 없는 경우 1로 대체
+    obj[clothes[i][1]] ? (obj[clothes[i][1]] += 1) : (obj[clothes[i][1]] = 1);
   }
-  arr.splice(number.length - k, k);
-  return arr.join("");
+
+  for (let key in obj) {
+    // 카테고리에 해당하는 의상을 입지 않을 경우 포함 +1
+    answer *= obj[key] + 1;
+  }
+
+  // 문제 조건에서 아무 의상도 입지 않을 경우는 제외했기 때문에 -1
+  return answer - 1;
 }
 
-console.log(solution("1924", 2));
-console.log(solution("1231234", 3));
-console.log(solution("4177252841", 4));
+console.log(
+  solution([
+    ["yellow_hat", "headgear"],
+    ["blue_sunglasses", "eyewear"],
+    ["green_turban", "headgear"],
+  ])
+);
+console.log(
+  solution([
+    ["crow_mask", "face"],
+    ["blue_sunglasses", "face"],
+    ["smoky_makeup", "face"],
+  ])
+);
