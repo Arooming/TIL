d59fa37a976264f0eed023bb3bbf8dbf51c3e20b
seoAreum <1971236@hansung.ac.kr>
2023-09-27 16:18:19 +0900

    review: 09/27 TIL

--- programmers/review.js
+++ programmers/review.js
@@ -1,34 +1,24 @@
-// 의상
-function solution(clothes) {
-  var answer = 1;
-  let obj = {};
+// 큰 수 만들기
+function solution(number, k) {
+  let stack = [];
 
-  for (let i = 0; i < clothes.length; i++) {
-    // 카테고리에 해당하는 의상이 있는 경우 +1
-    // 카테고리에 해당하는 의상이 없는 경우 1로 대체
-    obj[clothes[i][1]] ? (obj[clothes[i][1]] += 1) : (obj[clothes[i][1]] = 1);
+  for (let i = 0; i < number.length; i++) {
+    while (k > 0 && stack[stack.length - 1] < number[i]) {
+      stack.pop();
+      k--;
+    }
+
+    stack.push(number[i]);
   }
 
-  for (let key in obj) {
-    // 카테고리에 해당하는 의상을 입지 않을 경우 포함 +1
-    answer *= obj[key] + 1;
-  }
+  // 위의 for문이 끝났음에도 k값이 남아있는 경우, 가장 상위 값부터 남은 k개의 수 제거
+  // ex. (1010, 2)
+  stack.splice(stack.length - k, k);
 
-  // 문제 조건에서 아무 의상도 입지 않을 경우는 제외했기 때문에 -1
-  return answer - 1;
+  return stack.join("");
 }
 
-console.log(
-  solution([
-    ["yellow_hat", "headgear"],
-    ["blue_sunglasses", "eyewear"],
-    ["green_turban", "headgear"],
-  ])
-);
-console.log(
-  solution([
-    ["crow_mask", "face"],
-    ["blue_sunglasses", "face"],
-    ["smoky_makeup", "face"],
-  ])
-);
+console.log(solution("1924", 2));
+console.log(solution("1231234", 3));
+console.log(solution("4177252841", 4));
+console.log(solution("1010", 2));
