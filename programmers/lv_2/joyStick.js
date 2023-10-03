5a97a84c44a7909a79966cb94819d5cc157a6d9c
seoAreum <1971236@hansung.ac.kr>
2023-10-03 16:57:41 +0900

    Update joyStick.js

--- programmers/lv_2/joyStick.js
+++ programmers/lv_2/joyStick.js
@@ -1,6 +1,32 @@
 // 조이스틱
 function solution(name) {
   let answer = 0;
+  let minMove = name.length - 1;
+
+  [...name].map((n, i) => {
+    answer += Math.min(n.charCodeAt() - 65, 91 - n.charCodeAt());
+    let idx = i + 1;
+
+    while (name[idx] === "A" && idx < name.length) {
+      idx++;
+    }
+
+    minMove = Math.min(
+      minMove,
+      i * 2 + name.length - idx,
+      i + 2 * (name.length - idx)
+    );
+  });
+
+  return answer + minMove;
+}
+
+console.log(solution("JEROEN"));
+console.log(solution("JAN"));
+
+/* 다른 풀이
+function solution(name) {
+  let answer = 0;
   const min = 65;
   const max = 90 + 1;
 
@@ -27,6 +53,4 @@ function solution(name) {
 
   return answer + minMove;
 }
-
-console.log(solution("JEROEN"));
-console.log(solution("JAN"));
+*/
