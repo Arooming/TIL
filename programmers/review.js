4b2b186afb676b9cc791a88d924e7c13b2c67adf
seoAreum <1971236@hansung.ac.kr>
2023-10-07 15:35:15 +0900

    review: 10/07 TIL

--- programmers/review.js
+++ programmers/review.js
@@ -1,63 +1,45 @@
-// 게임 맵 최단거리
-function solution(maps) {
-  let queue = [];
-  let visited = Array(maps.length)
-    .fill(0)
-    .map(() => Array(maps[0].length).fill(0));
-  let dist = maps;
+// 네트워크
+function solution(n, computers) {
+  let answer = 0;
+  let visited = Array(n).fill(0);
 
-  const n = maps.length;
-  const m = maps[0].length;
-  const dx = [0, 0, -1, 1];
-  const dy = [-1, 1, 0, 0];
+  function dfs(now) {
+    // 방문한 적 있으면 return
+    if (visited[now] === 1) {
+      return;
+    }
 
-  queue.push([0, 0]);
-  visited[0][0] = 1;
-
-  while (queue.length) {
-    let [x, y] = queue.shift();
-
-    for (let i = 0; i < 4; i++) {
-      let nx = x + dx[i];
-      let ny = y + dy[i];
-
-      if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
-        continue;
-      }
-
-      // 반드시 범위체크를 먼저하고 벽에 부딪혔는지 여부 체크할 것!
-      // 범위체크를 하지 않고 벽에 부딪혔는지부터 체크하면 에러 발생함;
-      if (maps[nx][ny] === 0) {
-        continue;
-      }
-
-      if (!visited[nx][ny]) {
-        queue.push([nx, ny]);
-        visited[nx][ny] = 1;
-        dist[nx][ny] = dist[x][y] + 1;
+    visited[now] = 1;
+    for (let i = 0; i < computers.length; i++) {
+      // 컴퓨터들이 연결되어 있으면 함수 재귀
+      if (computers[now][i] === 1) {
+        dfs(i);
       }
     }
   }
 
-  return dist[n - 1][m - 1] === 1 ? -1 : dist[n - 1][m - 1];
+  for (let i = 0; i < n; i++) {
+    // 방문한 적 없으면, 함수 재귀 + 네트워크 개수 추가
+    if (!visited[i]) {
+      dfs(i);
+      answer++;
+    }
+  }
+
+  return answer;
 }
 
 console.log(
-  solution([
-    [1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 1],
-    [0, 0, 0, 0, 1],
+  solution(3, [
+    [1, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1],
   ])
 );
-
 console.log(
-  solution([
-    [1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 1],
+  solution(3, [
+    [1, 1, 0],
+    [1, 1, 1],
+    [0, 1, 1],
   ])
 );
