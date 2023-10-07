9c8c2ab3933eb2c6b9ecc30409200ea9baf0dec3
seoAreum <1971236@hansung.ac.kr>
2023-10-07 15:34:31 +0900

    review: 10/07 TIL

--- programmers/review.js
+++ programmers/review.js
@@ -1,25 +1,63 @@
-// 조이스틱
-function solution(name) {
-  let answer = 0;
-  let minMove = name.length - 1;
+// 게임 맵 최단거리
+function solution(maps) {
+  let queue = [];
+  let visited = Array(maps.length)
+    .fill(0)
+    .map(() => Array(maps[0].length).fill(0));
+  let dist = maps;
 
-  [...name].map((n, i) => {
-    answer += Math.min(n.charCodeAt() - 65, 91 - n.charCodeAt());
-    let idx = i + 1;
+  const n = maps.length;
+  const m = maps[0].length;
+  const dx = [0, 0, -1, 1];
+  const dy = [-1, 1, 0, 0];
 
-    while (name[idx] === "A" && idx > i) {
-      idx++;
+  queue.push([0, 0]);
+  visited[0][0] = 1;
+
+  while (queue.length) {
+    let [x, y] = queue.shift();
+
+    for (let i = 0; i < 4; i++) {
+      let nx = x + dx[i];
+      let ny = y + dy[i];
+
+      if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+        continue;
+      }
+
+      // 반드시 범위체크를 먼저하고 벽에 부딪혔는지 여부 체크할 것!
+      // 범위체크를 하지 않고 벽에 부딪혔는지부터 체크하면 에러 발생함;
+      if (maps[nx][ny] === 0) {
+        continue;
+      }
+
+      if (!visited[nx][ny]) {
+        queue.push([nx, ny]);
+        visited[nx][ny] = 1;
+        dist[nx][ny] = dist[x][y] + 1;
+      }
     }
+  }
 
-    minMove = Math.min(
-      minMove,
-      i * 2 + name.length - idx,
-      i + 2 * (name.length - idx)
-    );
-  });
-
-  return answer + minMove;
+  return dist[n - 1][m - 1] === 1 ? -1 : dist[n - 1][m - 1];
 }
 
-console.log(solution("JEROEN"));
-console.log(solution("JAN"));
+console.log(
+  solution([
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 1],
+    [0, 0, 0, 0, 1],
+  ])
+);
+
+console.log(
+  solution([
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 1],
+  ])
+);
