cc55ccdebfffafc347eca25507b93c5962dbbc8b
seoAreum <1971236@hansung.ac.kr>
2023-10-10 12:54:02 +0900

    review: 10/10 TIL

--- programmers/review.js
+++ programmers/review.js
@@ -1,45 +1,54 @@
-// 네트워크
-function solution(n, computers) {
-  let answer = 0;
-  let visited = Array(n).fill(0);
+// 소수 찾기
+// 순열을 구하는 함수
+const getPermutation = (arr, selectNumber) => {
+  const result = [];
+  if (selectNumber === 1) return arr.map((v) => [v]);
+  else {
+    arr.forEach((fixed, index, origin) => {
+      const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
 
-  function dfs(now) {
-    // 방문한 적 있으면 return
-    if (visited[now] === 1) {
-      return;
-    }
+      const permutations = getPermutation(rest, selectNumber - 1);
 
-    visited[now] = 1;
-    for (let i = 0; i < computers.length; i++) {
-      // 컴퓨터들이 연결되어 있으면 함수 재귀
-      if (computers[now][i] === 1) {
-        dfs(i);
-      }
+      const attached = permutations.map((permu) => [fixed, ...permu]);
+      result.push(...attached);
+    });
+  }
+
+  return result;
+};
+
+// 소수 판별하는 함수
+const checkPrimeNumber = (number) => {
+  if (number < 2) return false;
+
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    const num = number % i;
+    if (num === 0) {
+      return false;
     }
   }
 
-  for (let i = 0; i < n; i++) {
-    // 방문한 적 없으면, 함수 재귀 + 네트워크 개수 추가
-    if (!visited[i]) {
-      dfs(i);
-      answer++;
-    }
+  return true;
+};
+
+function solution(numbers) {
+  const answer = new Set();
+
+  for (let i = 1; i <= numbers.length; i++) {
+    const permutation = [...getPermutation([...numbers], i)];
+
+    const primeNumbers = permutation.filter((arr) => {
+      const number = +arr.join("");
+      const isPrimeNumber = checkPrimeNumber(number);
+      return isPrimeNumber;
+    });
+
+    primeNumbers.forEach((arr) => answer.add(+arr.join("")));
   }
 
-  return answer;
+  return answer.size;
 }
 
-console.log(
-  solution(3, [
-    [1, 1, 0],
-    [1, 1, 0],
-    [0, 0, 1],
-  ])
-);
-console.log(
-  solution(3, [
-    [1, 1, 0],
-    [1, 1, 1],
-    [0, 1, 1],
-  ])
-);
+console.log(solution("17"));
+console.log(solution("011"));
+console.log(solution("123"));
