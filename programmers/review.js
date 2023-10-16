8aab72ee00d778477529d3fe9fa566e6bf378bb6
seoAreum <1971236@hansung.ac.kr>
2023-10-16 12:33:55 +0900

    review: 10/16 완주하지 못한 선수

--- programmers/review.js
+++ programmers/review.js
@@ -1,20 +1,22 @@
-// 폰켓몬
-function solution(nums) {
-  let arr = [];
-  arr.push(nums[0]);
-  let answer = 1;
-  for (let i = 0; i < nums.length; i++) {
-    if (answer < nums.length / 2) {
-      if (!arr.includes(nums[i])) {
-        arr.push(nums[i]);
-        answer++;
-      }
+// 완주하지 못한 선수
+function solution(participant, completion) {
+  participant.sort();
+  completion.sort();
+
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      return participant[i];
     }
   }
-
-  return answer;
 }
 
-console.log(solution([3, 1, 2, 3]));
-console.log(solution([3, 3, 3, 2, 2, 4]));
-console.log(solution([3, 3, 3, 2, 2, 2]));
+console.log(solution(["leo", "kiki", "eden"], ["eden", "kiki"]));
+console.log(
+  solution(
+    ["marina", "josipa", "nikola", "vinko", "filipa"],
+    ["josipa", "filipa", "marina", "nikola"]
+  )
+);
+console.log(
+  solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
+);
