53e824a5cbcd63e806b945fc06693110e8540044
seoAreum <1971236@hansung.ac.kr>
2023-10-19 17:08:31 +0900

    review: 10/19 k번째 수

--- programmers/review.js
+++ programmers/review.js
@@ -1,31 +1,23 @@
-// 의상
-function solution(clothes) {
-  var answer = 1;
-  let obj = {};
-  clothes.sort();
+// k 번째 수
+function solution(array, commands) {
+  var answer = [];
 
-  for (let i = 0; i < clothes.length; i++) {
-    obj[clothes[i][1]] = (obj[clothes[i][1]] || 1) + 1;
+  for (let i = 0; i < commands.length; i++) {
+    let slicedArr = array.slice(commands[i][0] - 1, commands[i][1]);
+    slicedArr.sort((a, b) => a - b);
+    answer.push(slicedArr[commands[i][2] - 1]);
   }
 
-  for (let key in obj) {
-    answer *= obj[key];
-  }
-
-  return answer - 1;
+  return answer;
 }
 
 console.log(
-  solution([
-    ["yellow_hat", "headgear"],
-    ["blue_sunglasses", "eyewear"],
-    ["green_turban", "headgear"],
-  ])
-);
-console.log(
-  solution([
-    ["crow_mask", "face"],
-    ["blue_sunglasses", "face"],
-    ["smoky_makeup", "face"],
-  ])
+  solution(
+    [1, 5, 2, 6, 3, 7, 4],
+    [
+      [2, 5, 3],
+      [4, 4, 1],
+      [1, 7, 3],
+    ]
+  )
 );
