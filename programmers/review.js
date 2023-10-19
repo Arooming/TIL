ac3f69b094fbb706b784a4bbd042e874623020b9
seoAreum <1971236@hansung.ac.kr>
2023-10-19 18:22:47 +0900

    review: 10/19 프로세스

--- programmers/review.js
+++ programmers/review.js
@@ -1,16 +1,27 @@
-// H-Index
-function solution(citations) {
+// 프로세스
+function solution(priorities, location) {
   var answer = 0;
+  let shiftedVal = 0;
 
-  citations.sort((a, b) => b - a);
+  let obj = priorities.map((prio, i) => {
+    return { prio: prio, idx: i };
+  });
+  let maxNum = Math.max(...obj.map((item) => item.prio));
 
-  for (let i = 0; i < citations.length; i++) {
-    if (citations[i] > i) {
-      answer++;
+  while (obj) {
+    maxNum = Math.max(...obj.map((item) => item.prio));
+    if (obj[0].prio < maxNum) {
+      shiftedVal = obj.shift();
+      obj.push(shiftedVal);
+    } else {
+      shiftedVal = obj.shift();
+      answer += 1;
+      if (shiftedVal.idx === location) {
+        return answer;
+      }
     }
   }
-
-  return answer;
 }
 
-console.log(solution([3, 0, 6, 1, 5]));
+console.log(solution([2, 1, 3, 2], 2));
+console.log(solution([1, 1, 9, 1, 1, 1], 0));
