e02331c37f43139edd33b87824b81d81be90aa92
seoAreum <1971236@hansung.ac.kr>
2024-02-20 12:19:24 +0900

    review: 02/20

--- programmers/review.js
+++ programmers/review.js
@@ -1,12 +1,18 @@
-// 가장 큰 수
-function solution(numbers) {
-  const answer = numbers
-    .map((it) => it.toString())
-    .sort((a, b) => b + a - (a + b))
-    .join("");
+// 전화번호 목록
+function solution(phone_book) {
+  const sortedBook = phone_book.sort();
+  let answer = false;
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (sortedBook[i + 1].slice(0, sortedBook[i].length) === sortedBook[i]) {
+      return false;
+    } else {
+      answer = true;
+    }
+  }
 
-  return answer[0] === "0" ? "0" : answer;
+  return answer;
 }
 
-console.log(solution([6, 10, 2]));
-console.log(solution([3, 30, 34, 5, 9]));
+console.log(solution(["119", "97674223", "1195524421"]));
+console.log(solution(["123", "456", "789"]));
+console.log(solution(["12", "123", "1235", "567", "88"]));
