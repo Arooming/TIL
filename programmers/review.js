0db944852837375f4b96877b67406cd4c6a4e9d6
seoAreum <1971236@hansung.ac.kr>
2024-02-20 12:44:27 +0900

    review: 02/20

--- programmers/review.js
+++ programmers/review.js
@@ -1,18 +1,24 @@
-// 전화번호 목록
-function solution(phone_book) {
-  const sortedBook = phone_book.sort();
-  let answer = false;
-  for (let i = 0; i < phone_book.length - 1; i++) {
-    if (sortedBook[i + 1].slice(0, sortedBook[i].length) === sortedBook[i]) {
-      return false;
-    } else {
-      answer = true;
+// 완주하지 못한 선수
+function solution(participant, completion) {
+  const sortedParticipant = participant.sort();
+  const sortedCompletion = completion.sort();
+
+  for (let i = 0; i < sortedCompletion.length; i++) {
+    if (sortedCompletion[i] !== sortedParticipant[i]) {
+      return sortedParticipant[i];
     }
   }
 
-  return answer;
+  return sortedParticipant[sortedParticipant.length - 1];
 }
 
-console.log(solution(["119", "97674223", "1195524421"]));
-console.log(solution(["123", "456", "789"]));
-console.log(solution(["12", "123", "1235", "567", "88"]));
+console.log(solution(["leo", "kiki", "eden"], ["eden", "kiki"]));
+console.log(
+  solution(
+    ["marina", "josipa", "nikola", "vinko", "filipa"],
+    ["josipa", "filipa", "marina", "nikola"]
+  )
+);
+console.log(
+  solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
+);
