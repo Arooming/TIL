b7b1140646d397853f6b296453e3c6e527ea12a4
seoAreum <1971236@hansung.ac.kr>
2024-02-26 12:17:26 +0900

    review: 02/26

--- programmers/review.js
+++ programmers/review.js
@@ -1,22 +1,33 @@
-// 같은 숫자는 싫어
-function solution(s) {
-  const stack = [];
-
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === "(") {
-      stack.push(s[i]);
-    } else {
-      if (stack.length === 0) {
-        return false;
-      }
-      stack.pop();
-    }
+// 의상
+function solution(clothes) {
+  let answer = 1;
+  const obj = {};
+  for (let i = 0; i < clothes.length; i++) {
+    obj.hasOwnProperty(clothes[i][1])
+      ? (obj[clothes[i][1]] += 1)
+      : (obj[clothes[i][1]] = 1);
   }
 
-  return stack.length === 0;
+  for (let key in obj) {
+    // 해당 종류의 의상을 입지 않는 경우까지 고려하여 +1
+    answer *= obj[key] + 1;
+  }
+
+  // 모든 의상 입지 않는 경우 제외
+  return answer - 1;
 }
 
-console.log(solution("()()"));
-console.log(solution("(())()"));
-console.log(solution(")()("));
-console.log(solution("(()("));
+console.log(
+  solution([
+    ["yellow_hat", "headgear"],
+    ["blue_sunglasses", "eyewear"],
+    ["green_turban", "headgear"],
+  ])
+);
+console.log(
+  solution([
+    ["crow_mask", "face"],
+    ["blue_sunglasses", "face"],
+    ["smoky_makeup", "face"],
+  ])
+);
