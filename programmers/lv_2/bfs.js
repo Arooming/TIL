076c907e6575fadf3ee3204b3be61a519be84e86
seoAreum <1971236@hansung.ac.kr>
2024-03-09 13:47:27 +0900

    review: 03/09

--- programmers/lv_2/bfs.js
+++ programmers/lv_2/bfs.js
@@ -1,64 +1,59 @@
 // 게임 맵 최단거리
 function solution(maps) {
-  // 정점을 넣을 배열
+  const N = maps.length;
+  const M = maps[0].length;
   let queue = [];
 
-  // maps의 크기만큼 0으로 채워넣은 2차원 배열
-  let visited = Array(maps.length)
+  // 방문 체크용 배열
+  let visited = Array(N)
     .fill(0)
-    .map(() => Array(maps[0].length).fill(0));
+    .map(() => Array(M).fill(0));
 
-  // 최단 거리를 나타낼 배열
+  // 최단거리 계산용 배열
   let dist = maps;
 
-  const n = maps.length;
-  const m = maps[0].length;
+  const rx = [0, 0, -1, 1];
+  const ry = [-1, 1, 0, 0];
 
-  // 상하좌우 탐색 시, 사용할 배열
-  const dx = [0, 0, -1, 1];
-  const dy = [-1, 1, 0, 0];
-
+  // 정점 방문
   queue.push([0, 0]);
+  // 방문했음을 체크
   visited[0][0] = true;
 
   while (queue.length) {
-    // 큐에 맵 시작 정점 넣기
+    // 현재 정점 꺼내기
     let [x, y] = queue.shift();
 
     // 상하좌우 탐색
     for (let i = 0; i < 4; i++) {
-      // 다음 정점 = 현재 정점 + 상하/좌우 한 칸씩 이동
-      let nx = x + dx[i];
-      let ny = y + dy[i];
+      const nx = x + rx[i];
+      const ny = y + ry[i];
 
-      // maps의 범위를 벗어난 경우 예외처리
-      if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+      // 예외처리 - 맵 밖으로 나가는 경우
+      if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
         continue;
       }
 
-      // 벽에 부딪힌 경우 예외처리 - 반드시 범위에 대한 예외처리 후에 처리해줄 것 !!
-      // 벽에 부딪힌 경우에 대한 예외처리를 범위에 대한 예외처리보다 먼저 해줄 경우, 범위를 벗어난 maps[nx][ny]는 unddefined를 뱉기 때문에 에러 발생!
+      // 예외처리 - 벽을 만난 경우
       if (maps[nx][ny] === 0) {
         continue;
       }
 
-      // 이동할 수 있는 경우(아직 방문하지 않은 위치인 경우)
+      // 방문처리가 되지 않은 경우 (방문하지 않은 정점)
       if (!visited[nx][ny]) {
-        // 방문 체크
-        visited[nx][ny] = 1;
-        // 큐에 해당 정점 넣기 -> 이 정점에 대해 방문했음을 저장!
+        visited[nx][ny] = true;
+        // 방문한 정점을 큐에 넣기
         queue.push([nx, ny]);
-        // 다음 정점의 최단거리 = 현재 정점의 최단거리 + 1
+        // 다음 정점의 최단거리 구하기
         dist[nx][ny] = dist[x][y] + 1;
       }
     }
   }
 
-  // 상대 팀 진영에 도착하지 못한 경우(지나갈 수 있는 칸이 없는 경우), -1 반환
-  // 상대 팀 진영에 도착할 수 있는 경우, 최단 거리 반환
-  return dist[n - 1][m - 1] === 1 ? -1 : dist[n - 1][m - 1];
+  // 최단거리 = dist 배열의 마지막 인덱스 값! (1부터 시작이라 마지막 값에 +1)
+  // 갈 수 없는 곳이면 -1을 리턴하라고 했으니까 조건 추가
+  return dist[N - 1][M - 1] === 1 ? -1 : dist[N - 1][M - 1];
 }
-
 console.log(
   solution([
     [1, 0, 1, 1, 1],
@@ -68,7 +63,6 @@ console.log(
     [0, 0, 0, 0, 1],
   ])
 );
-
 console.log(
   solution([
     [1, 0, 1, 1, 1],
--- programmers/review.js
+++ programmers/review.js
@@ -1,74 +1,52 @@
-// 게임 맵 최단거리
-function solution(maps) {
-  const N = maps.length;
-  const M = maps[0].length;
-  let queue = [];
+// 네트워크
+function solution(n, computers) {
+  // 얕은 복사 - computers 값이 바껴도 computer의 값은 바뀌지 않음!
+  const computer = computers;
+  // 방문 배열 만들기
+  let visited = Array(n).fill(0);
+  let answer = 0;
 
-  // 방문 체크용 배열
-  let visited = Array(N)
-    .fill(0)
-    .map(() => Array(M).fill(0));
+  // 현재 정점을 기준으로 깊이우선탐색
+  function dfs(now) {
+    // 이미 방문한 정점이면 return
+    if (visited[now]) {
+      return;
+    }
+    // 방문처리
+    visited[now] = true;
 
-  // 최단거리 계산용 배열
-  let dist = maps;
-
-  const rx = [0, 0, -1, 1];
-  const ry = [-1, 1, 0, 0];
-
-  // 정점 방문
-  queue.push([0, 0]);
-  // 방문했음을 체크
-  visited[0][0] = true;
-
-  while (queue.length) {
-    // 현재 정점 꺼내기
-    let [x, y] = queue.shift();
-
-    // 상하좌우 탐색
-    for (let i = 0; i < 4; i++) {
-      const nx = x + rx[i];
-      const ny = y + ry[i];
-
-      // 예외처리 - 맵 밖으로 나가는 경우
-      if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
-        continue;
-      }
-
-      // 예외처리 - 벽을 만난 경우
-      if (maps[nx][ny] === 0) {
-        continue;
-      }
-
-      // 방문처리가 되지 않은 경우 (방문하지 않은 정점)
-      if (!visited[nx][ny]) {
-        visited[nx][ny] = true;
-        // 방문한 정점을 큐에 넣기
-        queue.push([nx, ny]);
-        // 다음 정점의 최단거리 구하기
-        dist[nx][ny] = dist[x][y] + 1;
+    // 다음 방문할 정점이 있는지 탐색
+    for (let i = 0; i < computer.length; i++) {
+      // 컴퓨터끼리 연결되어 있을 때(1)만 탐색
+      if (computer[now][i] === 1) {
+        dfs(i);
       }
     }
   }
 
-  // 최단거리 = dist 배열의 마지막 인덱스 값! (1부터 시작이라 마지막 값에 +1)
-  // 갈 수 없는 곳이면 -1을 리턴하라고 했으니까 조건 추가
-  return dist[N - 1][M - 1] === 1 ? -1 : dist[N - 1][M - 1];
+  for (let i = 0; i < n; i++) {
+    // i번째 정점을 방문하지 않았다면
+    if (!visited[i]) {
+      dfs(i);
+      // 네트워크 개수 +1
+      answer++;
+    }
+  }
+
+  return answer;
 }
+
 console.log(
-  solution([
-    [1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 1],
-    [0, 0, 0, 0, 1],
+  solution(3, [
+    [1, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1],
   ])
 );
 console.log(
-  solution([
-    [1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 1],
+  solution(3, [
+    [1, 1, 0],
+    [1, 1, 1],
+    [0, 1, 1],
   ])
 );
