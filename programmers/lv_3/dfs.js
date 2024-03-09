8e638fe259ca8d584a8e3cf5bd4134ff4e73d7c6
seoAreum <1971236@hansung.ac.kr>
2024-03-09 13:48:50 +0900

    update: 03/09

--- programmers/lv_3/dfs.js
+++ programmers/lv_3/dfs.js
@@ -1,30 +1,34 @@
 // 네트워크
 function solution(n, computers) {
-  var answer = 0;
+  // 얕은 복사 - computers 값이 바껴도 computer의 값은 바뀌지 않음!
+  const computer = computers;
+  // 방문 배열 만들기
   let visited = Array(n).fill(0);
+  let answer = 0;
 
+  // 현재 정점을 기준으로 깊이우선탐색
   function dfs(now) {
-    // 현재 정점을 방문한 적 있다면 return;
+    // 이미 방문한 정점이면 return
     if (visited[now]) {
       return;
     }
+    // 방문처리
+    visited[now] = true;
 
-    // 방문 처리
-    visited[now] = 1;
-    for (let i = 0; i < computers.length; i++) {
-      // 현재 정점에서 네트워크가 연결되어 있는 경우
-      if (computers[now][i] === 1) {
+    // 다음 방문할 정점이 있는지 탐색
+    for (let i = 0; i < computer.length; i++) {
+      // 컴퓨터끼리 연결되어 있을 때(1)만 탐색
+      if (computer[now][i] === 1) {
         dfs(i);
       }
     }
   }
 
   for (let i = 0; i < n; i++) {
-    // 현재 정점에 아직 방문하지 않은 경우
+    // i번째 정점을 방문하지 않았다면
     if (!visited[i]) {
-      // 현재 정점에 대해 dfs() 재귀
       dfs(i);
-      // 네트워크 개수 ++
+      // 네트워크 개수 +1
       answer++;
     }
   }
