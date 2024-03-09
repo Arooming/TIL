5f12221ab203657ef7ea310b41780dad5349f481
seoAreum <1971236@hansung.ac.kr>
2024-03-09 14:24:07 +0900

    review: 03/09{

--- programmers/review.js
+++ programmers/review.js
@@ -1,52 +1,58 @@
-// 네트워크
-function solution(n, computers) {
-  // 얕은 복사 - computers 값이 바껴도 computer의 값은 바뀌지 않음!
-  const computer = computers;
-  // 방문 배열 만들기
-  let visited = Array(n).fill(0);
-  let answer = 0;
+// 게임 맵 최단거리
+function solution(maps) {
+  const n = maps.length;
+  const m = maps[0].length;
 
-  // 현재 정점을 기준으로 깊이우선탐색
-  function dfs(now) {
-    // 이미 방문한 정점이면 return
-    if (visited[now]) {
-      return;
-    }
-    // 방문처리
-    visited[now] = true;
+  const visited = Array(n)
+    .fill(0)
+    .map(() => Array(m).fill(0));
+  const queue = [];
+  let dist = maps;
 
-    // 다음 방문할 정점이 있는지 탐색
-    for (let i = 0; i < computer.length; i++) {
-      // 컴퓨터끼리 연결되어 있을 때(1)만 탐색
-      if (computer[now][i] === 1) {
-        dfs(i);
+  const rx = [0, 0, -1, 1];
+  const ry = [-1, 1, 0, 0];
+
+  queue.push([0, 0]);
+  visited[0][0] = true;
+
+  while (queue.length) {
+    let [x, y] = queue.shift();
+
+    for (let i = 0; i < 4; i++) {
+      const nx = x + rx[i];
+      const ny = y + ry[i];
+
+      if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+        continue;
+      }
+      if (maps[nx][ny] === 0) {
+        continue;
+      }
+      if (!visited[nx][ny]) {
+        queue.push([nx, ny]);
+        visited[nx][ny] = true;
+        dist[nx][ny] = dist[x][y] + 1;
       }
     }
   }
 
-  for (let i = 0; i < n; i++) {
-    // i번째 정점을 방문하지 않았다면
-    if (!visited[i]) {
-      dfs(i);
-      // 네트워크 개수 +1
-      answer++;
-    }
-  }
-
-  return answer;
+  return dist[n - 1][m - 1] === 1 ? -1 : dist[n - 1][m - 1];
 }
-
 console.log(
-  solution(3, [
-    [1, 1, 0],
-    [1, 1, 0],
-    [0, 0, 1],
+  solution([
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 1],
+    [0, 0, 0, 0, 1],
   ])
 );
 console.log(
-  solution(3, [
-    [1, 1, 0],
-    [1, 1, 1],
-    [0, 1, 1],
+  solution([
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 1],
   ])
 );
