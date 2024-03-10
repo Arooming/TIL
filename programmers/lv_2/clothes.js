ffa37cf84b4409fccf91e54aa5ff25e21c08eb09
seoAreum <1971236@hansung.ac.kr>
2024-03-11 01:41:52 +0900

    update: 03/10

--- programmers/lv_2/clothes.js
+++ programmers/lv_2/clothes.js
@@ -1,23 +1,23 @@
 // 의상
 function solution(clothes) {
   let answer = 1;
-  let obj = {};
+  const obj = {};
 
   for (let i = 0; i < clothes.length; i++) {
-    // obj[key] = value;
-    // 카테고리에 해당하는 의상이 없는 경우, 1로 대체
-    // 카테고리에 해당하는 의상이 있는 경우, 의상의 개수 1 증가
-    // 의상을 입지 않는 경우도 고려하여 +1
-    obj[clothes[i][1]] = (obj[clothes[i][1]] || 1) + 1;
+    // clothes[i][1]에 해당하는 의상이 있는 경우, 해당 종류의 의상 개수 +1
+    // clothes[i][1]에 해당하는 의상이 없는 경우, 1로 초기화
+    // 아무것도 입지 않는 경우를 고려하여 +1
+    // obj[clothes[i][1]] = (obj[clothes[i][1]] || 1) + 1;
+    obj[clothes[i][1]] =
+      (obj.hasOwnProperty(clothes[i][1]) ? obj[clothes[i][1]]++ : 1) + 1;
   }
 
   for (let key in obj) {
-    // key는 obj의 key 값
-    // obj[key]는 obj의 value 값
+    // 서로 다른 옷 조합 계산
     answer *= obj[key];
   }
 
-  // 의상을 모두 입지 않는 경우는 제외
+  // 아무 것도 입지 않는 경우 제외하기 위해 -1
   return answer - 1;
 }
 
