f58c3d6412e6e38f1c264afef0185535b68ef4f7
seoAreum <1971236@hansung.ac.kr>
2024-03-11 01:05:17 +0900

    review: 03/10

--- programmers/review.js
+++ programmers/review.js
@@ -1,58 +1,88 @@
-// 게임 맵 최단거리
-function solution(maps) {
-  const n = maps.length;
-  const m = maps[0].length;
+// 더 맵게
+// 해당 문제는 Heap 구조를 활용해야 함
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
 
-  const visited = Array(n)
-    .fill(0)
-    .map(() => Array(m).fill(0));
-  const queue = [];
-  let dist = maps;
+  size() {
+    return this.heap.length;
+  }
 
-  const rx = [0, 0, -1, 1];
-  const ry = [-1, 1, 0, 0];
+  peek() {
+    return this.heap[0];
+  }
 
-  queue.push([0, 0]);
-  visited[0][0] = true;
+  push(value) {
+    this.heap.push(value);
 
-  while (queue.length) {
-    let [x, y] = queue.shift();
+    // 힙의 가장 마지막 인덱스
+    let curIdx = this.heap.length - 1;
 
-    for (let i = 0; i < 4; i++) {
-      const nx = x + rx[i];
-      const ny = y + ry[i];
+    // curIdx가 0보다 크고 && curIdx의 데이터보다 부모 인덱스의 데이터 값이 더 큰 경우 반복문 실행
+    while (
+      curIdx > 0 &&
+      this.heap[curIdx] < this.heap[Math.floor((curIdx - 1) / 2)]
+    ) {
+      const temp = this.heap[curIdx];
+      this.heap[curIdx] = this.heap[Math.floor((curIdx - 1) / 2)];
+      this.heap[Math.floor((curIdx - 1) / 2)] = temp;
 
-      if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
-        continue;
-      }
-      if (maps[nx][ny] === 0) {
-        continue;
-      }
-      if (!visited[nx][ny]) {
-        queue.push([nx, ny]);
-        visited[nx][ny] = true;
-        dist[nx][ny] = dist[x][y] + 1;
-      }
+      curIdx = Math.floor((curIdx - 1) / 2);
     }
   }
 
-  return dist[n - 1][m - 1] === 1 ? -1 : dist[n - 1][m - 1];
+  pop() {
+    if (this.heap.length === 0) return null;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    // 부모 데이터 꺼내오기
+    const minValue = this.heap[0];
+    this.heap[0] = this.heap.pop();
+
+    let curIdx = 0;
+
+    // 왼쪽 자식 인덱스가 힙의 크기보다 작은 경우 반복
+    while (curIdx * 2 + 1 < this.heap.length) {
+      // 오른쪽 자식 인덱스가 힙의 크기보다 작고 && 오른쪽 자식 값이 왼쪽 자식 값보다 작은 경우
+      // minChildIdx = 오른쪽 자식 값, 그렇지 않은 경우 왼쪽 자식 값으로 초기화
+      const minChildIdx =
+        curIdx * 2 + 2 < this.heap.length &&
+        this.heap[curIdx * 2 + 2] < this.heap[curIdx * 2 + 1]
+          ? curIdx * 2 + 2
+          : curIdx * 2 + 1;
+
+      // curIdx에 속한 값이 minChildIdx보다 작은 경우 반복문 탈출
+      if (this.heap[curIdx] < this.heap[minChildIdx]) break;
+
+      const temp = this.heap[curIdx];
+      this.heap[curIdx] = this.heap[minChildIdx];
+      this.heap[minChildIdx] = temp;
+
+      curIdx = minChildIdx;
+    }
+
+    return minValue;
+  }
 }
-console.log(
-  solution([
-    [1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 1],
-    [0, 0, 0, 0, 1],
-  ])
-);
-console.log(
-  solution([
-    [1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 1],
-  ])
-);
+
+function solution(scoville, K) {
+  const minHeap = new MinHeap();
+  let cnt = 0;
+
+  for (const sco of scoville) {
+    minHeap.push(sco);
+  }
+
+  while (minHeap.size() >= 2 && minHeap.peek() < K) {
+    const first = minHeap.pop();
+    const second = minHeap.pop();
+    const mixed = first + second * 2;
+    minHeap.push(mixed);
+    cnt++;
+  }
+
+  return minHeap.peek() < K ? -1 : cnt;
+}
+
+console.log(solution([1, 2, 3, 9, 10, 12], 7));
