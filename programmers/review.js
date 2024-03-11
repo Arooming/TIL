72de6d76f5d263121c1d60965afe1604a90c185d
seoAreum <1971236@hansung.ac.kr>
2024-03-11 20:54:25 +0900

    review: 03/11

--- programmers/review.js
+++ programmers/review.js
@@ -1,32 +1,62 @@
-// 의상
-function solution(clothes) {
-  let answer = 1;
-  const obj = {};
+// 게임 맵 최단거리
+function solution(maps) {
+  const n = maps.length;
+  const m = maps[0].length;
 
-  for (let i = 0; i < clothes.length; i++) {
-    obj[clothes[i][1]] =
-      (obj.hasOwnProperty(clothes[i][1]) ? obj[clothes[i][1]]++ : 1) + 1;
+  const dist = maps;
+  const queue = [];
+  const visited = Array(n)
+    .fill(0)
+    .map(() => Array(m).fill(0));
+
+  const rx = [0, 0, -1, 1];
+  const ry = [-1, 1, 0, 0];
+
+  queue.push([0, 0]);
+  visited[0][0] = true;
+
+  while (queue.length) {
+    let [x, y] = queue.shift();
+
+    for (let i = 0; i < 4; i++) {
+      const nx = rx[i] + x;
+      const ny = ry[i] + y;
+
+      if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+        continue;
+      }
+
+      if (maps[nx][ny] === 0) {
+        continue;
+      }
+
+      if (!visited[nx][ny]) {
+        queue.push([nx, ny]);
+        visited[nx][ny] = true;
+        dist[nx][ny] = dist[x][y] + 1;
+      }
+    }
   }
 
-  for (let key in obj) {
-    answer *= obj[key];
-  }
-
-  return answer - 1;
+  return dist[n - 1][m - 1] === 1 ? -1 : dist[n - 1][m - 1];
 }
 
 console.log(
   solution([
-    ["yellow_hat", "headgear"],
-    ["blue_sunglasses", "eyewear"],
-    ["green_turban", "headgear"],
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 1],
+    [0, 0, 0, 0, 1],
   ])
 );
 
 console.log(
   solution([
-    ["crow_mask", "face"],
-    ["blue_sunglasses", "face"],
-    ["smoky_makeup", "face"],
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 1],
   ])
 );
