3c9ac7e49c8b9bdff98f4d912e4dc7f6e96c8b54
seoAreum <1971236@hansung.ac.kr>
2024-03-25 20:02:36 +0900

    Update: 03/25

--- programmers/lv_2/primeNum.js
+++ programmers/lv_2/primeNum.js
@@ -1,60 +1,53 @@
-// 소수 찾기 (순열 활용)
-// 순열: n개 중 r개를 순서대로 뽑아서 줄 세우기
-// 순열을 구하는 함수
-const getPermutation = (arr, selectNumber) => {
-  const results = [];
-  // 선택해야하는 숫자가 1개일 때, 모든 배열의 원소 반환
-  if (selectNumber === 1) return arr.map((v) => [v]);
-  else {
-    arr.forEach((fixed, index, origin) => {
-      // 자기 자신을 제외한 모든 숫자 포함
-      const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
-
-      // 재귀적으로 함수 호출
-      const permutations = getPermutation(rest, selectNumber - 1);
-
-      // 고정 숫자 하나와 순열의 모든 경우 합침
-      const attached = permutations.map((permutation) => [
-        fixed,
-        ...permutation,
-      ]);
-      results.push(...attached);
-    });
-  }
-
-  return results;
-};
-
-// 소수 판별하는 함수
-const checkPrimeNumber = (number) => {
-  if (number < 2) return false;
-
-  // 소수 판별하는 방법 중 속도가 가장 빠른 방법: 2부터 N의 제곱근까지의 수로 N을 나누었을 때 나머지가 0이 되지 않는 것 찾기
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    const remainder = number % i;
-    // 나머지가 0인 경우, 소수가 아님
-    if (remainder === 0) return false;
-  }
-  return true;
-};
-
+// 소수찾기
 function solution(numbers) {
-  // Set()을 활용하여 중복제거 (011과 11은 동일 -> 중복 제거)
+  const getPermutation = (arr, selectNum) => {
+    const result = [];
+    // 재귀를 탈출하는 조건: 선택해야 하는 숫자가 1개일 때
+    if (selectNum === 1) return arr.map((v) => [v]);
+
+    // 재귀적으로 함수를 호출, 고정한 숫자 하나와 순열의 모든 경우를 하나의 배열에 합치기
+    arr.forEach((fixed, idx, origin) => {
+      // 조합을 구하고 싶은 경우, 아래와 같이 rest만 초기화 해주면 됨.
+      // const rest = [...origin.slice(idx + 1)];
+
+      // 자기 자신을 제외한 모든 수를 포함한 배열 (순열), 해당하는 fixed를 제외한 나머지 배열
+      const rest = [...origin.slice(0, idx), ...origin.slice(idx + 1)];
+      // 나머지에 대한 순열 구하기
+      const permutation = getPermutation(rest, selectNum - 1);
+      // 돌아온 순열에 떼어놨던 fixed 값 붙이기
+      const attatched = permutation.map((el) => [fixed, ...el]);
+      //  spread syntax로 배열화하여 모두 push
+      result.push(...attatched);
+    });
+
+    return result;
+  };
+
+  // 1은 소수가 아니므로, 1인 경우와 나머지 가 0인 경우 false 반환, 그렇지 않다면 true 반환
+  const checkPrimeNum = (number) => {
+    if (number < 2) return false;
+
+    // N을 2부터 N의 제곱근으로 나눴을 때, 나머지가 0이 아닌 수를 찾는 것이 가장 속도가 빠름
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      const remainder = number % i;
+      if (remainder === 0) return false;
+    }
+
+    return true;
+  };
+
+  // 011, 11과 같은 중복 숫자 방지를 위해 Set() 사용
   const answer = new Set();
 
   for (let i = 1; i <= numbers.length; i++) {
-    // 순열 구하기
     const permutation = [...getPermutation([...numbers], i)];
-    const primeNumbers = permutation.filter((arr) => {
-      // 숫자 조합
+    const primeNum = permutation.filter((arr) => {
       const number = +arr.join("");
-      // 소수 체크
-      const isPrimeNumber = checkPrimeNumber(number);
+      const isPrimeNumber = checkPrimeNum(number);
       return isPrimeNumber;
     });
 
-    primeNumbers.forEach((arr) => {
-      // arr을 문자열 형태로 바꿔서 answer에 추가
+    primeNum.forEach((arr) => {
       answer.add(+arr.join(""));
     });
   }
