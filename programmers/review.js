075bde5eb7b6f375f51fa86d508d902b8dc55bea
seoAreum <1971236@hansung.ac.kr>
2024-03-26 13:39:20 +0900

    review: 03/26

--- programmers/review.js
+++ programmers/review.js
@@ -1,60 +1,41 @@
-// 게임 맵 최단거리
-function solution(maps) {
-  const n = maps.length;
-  const m = maps[0].length;
+// 네트워크
+function solution(n, computers) {
+  const visited = Array(n).fill(0);
+  const computer = computers;
+  let answer = 0;
 
-  const dist = maps;
-  const queue = [];
-  const visited = Array(n)
-    .fill(0)
-    .map(() => Array(m).fill(0));
+  function dfs(now) {
+    if (visited[now]) return;
 
-  const dx = [0, 0, -1, 1];
-  const dy = [-1, 1, 0, 0];
-
-  queue.push([0, 0]);
-  visited[0][0] = true;
-
-  while (queue.length) {
-    const [x, y] = queue.shift();
-    for (let i = 0; i < 4; i++) {
-      const nx = dx[i] + x;
-      const ny = dy[i] + y;
-
-      if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
-        continue;
-      }
-
-      if (maps[nx][ny] === 0) {
-        continue;
-      }
-
-      if (!visited[nx][ny]) {
-        queue.push([nx, ny]);
-        visited[nx][ny] = true;
-        dist[nx][ny] = dist[x][y] + 1;
+    visited[now] = true;
+    for (let i = 0; i < computer.length; i++) {
+      if (computer[now][i] === 1) {
+        dfs(i);
       }
     }
   }
 
-  return dist[n - 1][m - 1] === 1 ? -1 : dist[n - 1][m - 1];
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      answer++;
+    }
+  }
+
+  return answer;
 }
 
 console.log(
-  solution([
-    [1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 1],
-    [0, 0, 0, 0, 1],
+  solution(3, [
+    [1, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1],
   ])
 );
 console.log(
-  solution([
-    [1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 1],
+  solution(3, [
+    [1, 1, 0],
+    [1, 1, 1],
+    [0, 1, 1],
   ])
 );
