0029d88a9044374f75385262b4abde250bb680b4
seoAreum <1971236@hansung.ac.kr>
2024-03-28 16:57:34 +0900

    review: 03/28

--- programmers/review.js
+++ programmers/review.js
@@ -1,79 +1,18 @@
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
+// 카펫
+function solution(brown, yellow) {
+  const sum = brown + yellow;
 
-  size() {
-    return this.heap.length;
-  }
+  for (let y = 3; y <= Math.sqrt(sum); y++) {
+    if (sum % y === 0) {
+      const x = sum / y;
 
-  peek() {
-    return this.heap[0];
-  }
-
-  push(value) {
-    this.heap.push(value);
-    let curIdx = this.heap.length - 1;
-
-    while (
-      curIdx > 0 &&
-      this.heap[curIdx] < this.heap[Math.floor((curIdx - 1) / 2)]
-    ) {
-      const temp = this.heap[curIdx];
-      this.heap[curIdx] = this.heap[Math.floor((curIdx - 1) / 2)];
-      this.heap[Math.floor((curIdx - 1) / 2)] = temp;
-
-      curIdx = Math.floor((curIdx - 1) / 2);
+      if ((x - 2) * (y - 2) === yellow) {
+        return [x, y];
+      }
     }
-  }
-
-  pop() {
-    if (this.heap.length === 0) return null;
-    if (this.heap.length === 1) return this.heap.pop();
-
-    const minValue = this.heap[0];
-    this.heap[0] = this.heap.pop();
-
-    let curIdx = 0;
-
-    while (curIdx * 2 + 1 < this.heap.length) {
-      const minChildIdx =
-        curIdx * 2 + 2 < this.heap.length &&
-        this.heap[curIdx * 2 + 2] < this.heap[curIdx * 2 + 1]
-          ? curIdx * 2 + 2
-          : curIdx * 2 + 1;
-
-      if (this.heap[minChildIdx] > this.heap[curIdx]) break;
-
-      const temp = this.heap[curIdx];
-      this.heap[curIdx] = this.heap[minChildIdx];
-      this.heap[minChildIdx] = temp;
-
-      curIdx = minChildIdx;
-    }
-
-    return minValue;
   }
 }
 
-function solution(scoville, K) {
-  const minHeap = new MinHeap();
-  let answer = 0;
-
-  for (const sco in scoville) {
-    minHeap.push(sco);
-  }
-
-  while (minHeap.peek() < K && minHeap.size() >= 2) {
-    const first = minHeap.pop();
-    const second = minHeap.pop();
-    const mixed = first + second * 2;
-
-    minHeap.push(mixed);
-    answer++;
-  }
-
-  return minHeap.peek() < K ? -1 : answer;
-}
-
-console.log(solution([1, 2, 3, 9, 10, 12], 7));
+console.log(solution(10, 2));
+console.log(solution(8, 1));
+console.log(solution(24, 24));
