b68baa3ac2686c81cafc59b12bb1505b8a7e7b0f
seoAreum <1971236@hansung.ac.kr>
2024-03-29 02:01:28 +0900

    review: 03/28

--- programmers/review.js
+++ programmers/review.js
@@ -1,18 +1,31 @@
 // 카펫
-function solution(brown, yellow) {
-  const sum = brown + yellow;
+function solution(k, dungeons) {
+  const visited = Array(dungeons.length).fill(0);
+  const dungeon = dungeons;
+  let answer = [];
 
-  for (let y = 3; y <= Math.sqrt(sum); y++) {
-    if (sum % y === 0) {
-      const x = sum / y;
+  const dfs = (k, count) => {
+    answer.push(count);
 
-      if ((x - 2) * (y - 2) === yellow) {
-        return [x, y];
+    for (let i = 0; i < dungeon.length; i++) {
+      let current = dungeon[i];
+      if (!visited[i] && k >= current[0]) {
+        visited[i] = 1;
+        dfs(k - current[1], count + 1);
+        visited[i] = 0;
       }
     }
-  }
+  };
+
+  dfs(k, 0);
+
+  return Math.max(...answer);
 }
 
-console.log(solution(10, 2));
-console.log(solution(8, 1));
-console.log(solution(24, 24));
+console.log(
+  solution(80, [
+    [80, 20],
+    [50, 40],
+    [30, 10],
+  ])
+);
