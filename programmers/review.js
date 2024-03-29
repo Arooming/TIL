4c79a7d27ca8bca298732d141b9678d1e0a7cc96
seoAreum <1971236@hansung.ac.kr>
2024-03-29 17:28:19 +0900

    review: 03/29

--- programmers/review.js
+++ programmers/review.js
@@ -1,63 +1,28 @@
-// 전력망을 둘로 나누기
-function solution(n, wires) {
-  let answer = Number.MAX_SAFE_INTEGER;
-  let cnt = 0;
+// 모음 사전
+function solution(word) {
+  const result = [];
+  const str = "";
 
-  function bfs(node) {
-    const visited = [];
-    const needVisitedNode = [node];
-
-    while (needVisitedNode.length) {
-      const cur = needVisitedNode.shift();
-
-      wires.forEach((wire) => {
-        if (wire.includes(cur)) {
-          const other = wire[0] === cur ? wire[1] : wire[0];
-          if (!visited.includes(other)) needVisitedNode.push(other);
-        }
-      });
-      visited.push(cur);
+  const dfs = (word, length, result) => {
+    const arr = [..."AEIOU"];
+    if (length === word.length) {
+      result.push(word);
+      return;
     }
-    return visited.length;
+
+    arr.forEach((it) => {
+      dfs(word + it, length, result);
+    });
+  };
+
+  for (let i = 1; i <= 5; i++) {
+    dfs(str, i, result);
   }
 
-  while (cnt !== wires.length) {
-    const [node1, node2] = wires.shift();
-
-    answer = Math.min(answer, Math.abs(bfs(node1) - bfs(node2)));
-    wires.push([node1, node2]);
-    cnt++;
-  }
-
-  return answer;
+  return result.sort().indexOf(word) + 1;
 }
 
-console.log(
-  solution(9, [
-    [1, 3],
-    [2, 3],
-    [3, 4],
-    [4, 5],
-    [4, 6],
-    [4, 7],
-    [7, 8],
-    [7, 9],
-  ])
-);
-console.log(
-  solution(4, [
-    [1, 2],
-    [2, 3],
-    [3, 4],
-  ])
-);
-console.log(
-  solution(7, [
-    [1, 2],
-    [2, 7],
-    [3, 7],
-    [3, 4],
-    [4, 5],
-    [6, 7],
-  ])
-);
+console.log(solution("AAAAE"));
+console.log(solution("AAAE"));
+console.log(solution("I"));
+console.log(solution("EIO"));
