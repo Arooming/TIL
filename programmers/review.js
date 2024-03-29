cfedddadcbd7b546631f2dba3a41d79f15ad3ced
seoAreum <1971236@hansung.ac.kr>
2024-03-29 16:33:09 +0900

    review: 03/29

--- programmers/review.js
+++ programmers/review.js
@@ -1,31 +1,63 @@
-// 카펫
-function solution(k, dungeons) {
-  const visited = Array(dungeons.length).fill(0);
-  const dungeon = dungeons;
-  let answer = [];
+// 전력망을 둘로 나누기
+function solution(n, wires) {
+  let answer = Number.MAX_SAFE_INTEGER;
+  let cnt = 0;
 
-  const dfs = (k, count) => {
-    answer.push(count);
+  function bfs(node) {
+    const visited = [];
+    const needVisitedNode = [node];
 
-    for (let i = 0; i < dungeon.length; i++) {
-      let current = dungeon[i];
-      if (!visited[i] && k >= current[0]) {
-        visited[i] = 1;
-        dfs(k - current[1], count + 1);
-        visited[i] = 0;
-      }
+    while (needVisitedNode.length) {
+      const cur = needVisitedNode.shift();
+
+      wires.forEach((wire) => {
+        if (wire.includes(cur)) {
+          const other = wire[0] === cur ? wire[1] : wire[0];
+          if (!visited.includes(other)) needVisitedNode.push(other);
+        }
+      });
+      visited.push(cur);
     }
-  };
+    return visited.length;
+  }
 
-  dfs(k, 0);
+  while (cnt !== wires.length) {
+    const [node1, node2] = wires.shift();
 
-  return Math.max(...answer);
+    answer = Math.min(answer, Math.abs(bfs(node1) - bfs(node2)));
+    wires.push([node1, node2]);
+    cnt++;
+  }
+
+  return answer;
 }
 
 console.log(
-  solution(80, [
-    [80, 20],
-    [50, 40],
-    [30, 10],
+  solution(9, [
+    [1, 3],
+    [2, 3],
+    [3, 4],
+    [4, 5],
+    [4, 6],
+    [4, 7],
+    [7, 8],
+    [7, 9],
+  ])
+);
+console.log(
+  solution(4, [
+    [1, 2],
+    [2, 3],
+    [3, 4],
+  ])
+);
+console.log(
+  solution(7, [
+    [1, 2],
+    [2, 7],
+    [3, 7],
+    [3, 4],
+    [4, 5],
+    [6, 7],
   ])
 );
