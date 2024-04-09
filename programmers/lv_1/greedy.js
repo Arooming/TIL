90d3e6fd0e9b48d67158a6bd7bb60ecdf1adb3d9
seoAreum <1971236@hansung.ac.kr>
2024-04-10 01:46:13 +0900

    Update: 체육복

--- programmers/lv_1/greedy.js
+++ programmers/lv_1/greedy.js
@@ -1,31 +1,17 @@
+// 체육복
 function solution(n, lost, reserve) {
-  // 1. 도난 당하고, 여벌 옷이 없는 사람 - 옷을 빌려야 함
-  const lostStud = lost
-    .sort((a, b) => a - b)
-    .filter((lost) => !reserve.includes(lost));
+  let lost_student = lost.filter((it) => !reserve.includes(it)).sort();
+  let reserve_student = reserve.filter((it) => !lost.includes(it)).sort();
 
-  // 2. 도난 당하지 않고, 여벌 옷이 있어서 옷을 빌려줄 수 있는 사람 - 옷을 빌려줄 수 있음
-  let reserveStud = reserve
-    .sort((a, b) => a - b)
-    .filter((reserve) => !lost.includes(reserve));
+  for (let i = 0; i < n; i++) {
+    if (lost_student.includes(reserve_student[i] - 1)) {
+      lost_student = lost_student.filter((it) => it !== reserve_student[i] - 1);
+    } else if (lost_student.includes(reserve_student[i] + 1)) {
+      lost_student = lost_student.filter((it) => it !== reserve_student[i] + 1);
+    }
+  }
 
-  // 3. 체육복을 빌리지 못해, 수업에 참여할 수 없는 학생 (최종)
-  const finalLost = lostStud.filter((lost) => {
-    // 체급차이가 1이라 체육복을 빌려줄 수 있는 학생
-    let canLendStud = reserveStud.find(
-      (canReserve) => Math.abs(lost - canReserve) === 1
-    );
-
-    // 체급차이가 1인 학생이 없으면 체육복을 빌릴 수 없음 -> 수업 참여 불가
-    if (!canLendStud) return lost;
-
-    // 여벌 옷을 빌려줄 수 있는 학생에서 이미 빌려준 학생은 제외
-    reserveStud = reserveStud.filter(
-      (canReserve) => canReserve !== canLendStud
-    );
-  });
-
-  return n - finalLost.length;
+  return n - lost_student.length;
 }
 
 console.log(solution(5, [2, 4], [1, 3, 5]));
