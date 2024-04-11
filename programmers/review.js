7badd092285f14d55c232488c8e25b7d4ea30214
seoAreum <1971236@hansung.ac.kr>
2024-04-11 15:13:45 +0900

    review: 04/11

--- programmers/review.js
+++ programmers/review.js
@@ -1,19 +1,27 @@
-// 체육복
-function solution(n, lost, reserve) {
-  let lost_student = lost.filter((it) => !reserve.includes(it)).sort();
-  let reserve_student = reserve.filter((it) => !lost.includes(it)).sort();
+// 피로도
+function solution(k, dungeons) {
+  const visited = Array(dungeons.length).fill(0);
+  let answer = 0;
 
-  for (let i = 0; i < n; i++) {
-    if (lost_student.includes(reserve_student[i] - 1)) {
-      lost_student = lost_student.filter((it) => it !== reserve_student[i] - 1);
-    } else if (lost_student.includes(reserve_student[i] + 1)) {
-      lost_student = lost_student.filter((it) => it !== reserve_student[i] + 1);
+  function dfs(cnt, now) {
+    for (let i = 0; i < dungeons.length; i++) {
+      if (!visited[i] && dungeons[i][0] <= now) {
+        visited[i] = true;
+        dfs(cnt + 1, now - dungeons[i][1]);
+        visited[i] = false;
+      }
     }
+    answer = Math.max(answer, cnt);
   }
+  dfs(0, k);
 
-  return n - lost_student.length;
+  return answer;
 }
 
-console.log(solution(5, [2, 4], [1, 3, 5]));
-console.log(solution(5, [2, 4], [3]));
-console.log(solution(3, [3], [1]));
+console.log(
+  solution(80, [
+    [80, 20],
+    [50, 40],
+    [30, 10],
+  ])
+);
