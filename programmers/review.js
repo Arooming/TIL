feffacfbdac827d71a5e271c90cac521e2c7909d
seoAreum <1971236@hansung.ac.kr>
2024-04-12 14:10:22 +0900

    review: 04/12

--- programmers/review.js
+++ programmers/review.js
@@ -1,25 +1,36 @@
-// 체육복
-function solution(n, lost, reserve) {
-  let lostStudent = lost.filter((lost) => !reserve.includes(lost)).sort();
-  const reserveStudent = reserve
-    .filter((reserve) => !lost.includes(reserve))
-    .sort();
+// 조이스틱
+function solution(name) {
+  const nameArr = [...name];
+  // 커서 이동 횟수: 문자열에 'A'가 없다면 이게 커서 최소 이동 횟수임.
+  let minMove = name.length - 1;
+  // 알파벳 이동 횟수
+  let answer = 0;
 
-  for (let i = 0; i < n; i++) {
-    if (lostStudent.includes(reserveStudent[i] - 1)) {
-      lostStudent = lostStudent.filter(
-        (lost) => lost !== reserveStudent[i] - 1
-      );
-    } else if (lostStudent.includes(reserveStudent[i] + 1)) {
-      lostStudent = lostStudent.filter(
-        (lost) => lost !== reserveStudent[i] + 1
-      );
+  nameArr.map((n, i) => {
+    answer += Math.min(
+      n.charCodeAt() - "A".charCodeAt(),
+      // Z의 아스키코드 + 1(A -> Z로 가는 횟수 1회)
+      "Z".charCodeAt() + 1 - n.charCodeAt()
+    );
+
+    let idx = i + 1;
+
+    // 연속되는 A의 개수 count
+    while (name[idx] === "A" && idx < name.length) {
+      idx++;
     }
-  }
 
-  return n - lostStudent.length;
+    // i: 현재 알파벳 앞의 알파벳 수, 2: 왔다 갔다하는 횟수, name.length - idx: 현재 알파벳 뒤의 알파벳 수
+    // minMove: 알파벳 순서대로 가는것, i * 2 + (name.length - idx): 뒤로 돌아가는 것, i + 2 * (name.length - idx): 맨 뒤 -> 맨 앞으로 와서 탐색하는 것
+    minMove = Math.min(
+      minMove,
+      i * 2 + (name.length - idx),
+      i + 2 * (name.length - idx)
+    );
+  });
+
+  return answer + minMove;
 }
 
-console.log(solution(5, [2, 4], [1, 3, 5]));
-console.log(solution(5, [2, 4], [3]));
-console.log(solution(3, [3], [1]));
+console.log(solution("JEROEN"));
+console.log(solution("JAN"));
