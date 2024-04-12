5971632816b92f79dd070e3d6962ae155b156f0c
seoAreum <1971236@hansung.ac.kr>
2024-04-12 14:11:50 +0900

    Update: 04/12

--- programmers/lv_2/joyStick.js
+++ programmers/lv_2/joyStick.js
@@ -1,26 +1,37 @@
 // 조이스틱
+// 조이스틱
 function solution(name) {
-  let answer = 0;
+  const nameArr = [...name];
+  // 커서 이동 횟수: 문자열에 'A'가 없다면 이게 커서 최소 이동 횟수임.
   let minMove = name.length - 1;
+  // 알파벳 이동 횟수
+  let answer = 0;
 
-  [...name].map((n, i) => {
-    answer += Math.min(n.charCodeAt() - 65, 91 - n.charCodeAt());
+  nameArr.map((n, i) => {
+    answer += Math.min(
+      n.charCodeAt() - "A".charCodeAt(),
+      // Z의 아스키코드 + 1(A -> Z로 가는 횟수 1회)
+      "Z".charCodeAt() + 1 - n.charCodeAt()
+    );
+
     let idx = i + 1;
 
+    // 연속되는 A의 개수 count
     while (name[idx] === "A" && idx < name.length) {
       idx++;
     }
 
+    // i: 현재 알파벳 앞의 알파벳 수, 2: 왔다 갔다하는 횟수, name.length - idx: 현재 알파벳 뒤의 알파벳 수
+    // minMove: 알파벳 순서대로 가는것, i * 2 + (name.length - idx): 뒤로 돌아가는 것, i + 2 * (name.length - idx): 맨 뒤 -> 맨 앞으로 와서 탐색하는 것
     minMove = Math.min(
       minMove,
-      i * 2 + name.length - idx,
+      i * 2 + (name.length - idx),
       i + 2 * (name.length - idx)
     );
   });
 
   return answer + minMove;
 }
-
 console.log(solution("JEROEN"));
 console.log(solution("JAN"));
 
