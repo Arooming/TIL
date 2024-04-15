666eef78f353f2a04e241024dea73929d5306359
seoAreum <1971236@hansung.ac.kr>
2024-04-15 11:23:28 +0900

    Update: 04/15

--- programmers/lv_2/maxNum2.js
+++ programmers/lv_2/maxNum2.js
@@ -1,16 +1,20 @@
 // 큰 수 만들기
 function solution(number, k) {
-    const arr = [];
-    for (let i = 0; i < number.length; i++) {
-      while (arr.length > 0 && arr[arr.length - 1] < number[i] && k > 0) {
-        k--;
-        arr.pop();
-      }
-      arr.push(number[i]);
+  const answer = [];
+
+  for (const i of number) {
+    while (k > 0 && answer[answer.length - 1] < i) {
+      answer.pop();
+      k--;
     }
-    arr.splice(number.length - k, k);
-    return arr.join("");
+    answer.push(i);
   }
+
+  // k가 0보다 큰 경우 뒤에서부터 k만큼 자르기
+  answer.splice(answer.length - k, k);
+
+  return answer.join("");
+}
 
 console.log(solution("1924", 2));
 console.log(solution("1231234", 3));
