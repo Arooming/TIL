35526fe04a36ea4c2d9fb4f499b53e4b4a3513fe
seoAreum <1971236@hansung.ac.kr>
2024-04-25 14:49:45 +0900

    review: 04/25

--- programmers/review.js
+++ programmers/review.js
@@ -1,32 +1,42 @@
-// 단어 변환
-function solution(begin, target, words) {
-  var answer = 0;
-  function checkWordCount(str1, str2) {
-    let cnt = 0;
-    for (let i = 0; i < str1.length; i++) {
-      if (str1[i] !== str2[i]) cnt++;
-    }
-    return cnt === 1;
-  }
+// 최소 직사각형
+function solution(sizes) {
+  const sortedSizes = sizes.map((size) =>
+    size[0] > size[1] ? [size[0], size[1]] : [size[1], size[0]]
+  );
+  const width = sortedSizes.map((sortedSize) => {
+    return sortedSize[0];
+  });
 
-  const queue = [begin];
-  const visited = {};
-  visited[begin] = 0;
+  const height = sortedSizes.map((sortedSize) => {
+    return sortedSize[1];
+  });
 
-  while (queue.length) {
-    const cur = queue.shift();
-
-    if (cur === target) break;
-
-    for (const word of words) {
-      if (checkWordCount(word, cur) && !visited[word]) {
-        queue.push(word);
-        visited[word] = visited[cur] + 1;
-      }
-    }
-  }
-  return visited[target] ? visited[target] : 0;
+  return Math.max(...width) * Math.max(...height);
 }
 
-console.log(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]));
-console.log(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log"]));
+console.log(
+  solution([
+    [60, 50],
+    [30, 70],
+    [60, 30],
+    [80, 40],
+  ])
+);
+console.log(
+  solution([
+    [10, 7],
+    [12, 3],
+    [8, 15],
+    [14, 7],
+    [5, 15],
+  ])
+);
+console.log(
+  solution([
+    [14, 4],
+    [19, 6],
+    [6, 16],
+    [18, 7],
+    [7, 11],
+  ])
+);
