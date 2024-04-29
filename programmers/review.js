3655e6f892e610aa62ecddfff75aa92319834147
seoAreum <1971236@hansung.ac.kr>
2024-04-29 16:04:14 +0900

    review: 04/29

--- programmers/review.js
+++ programmers/review.js
@@ -1,26 +1,17 @@
-// 조이스틱
-function solution(name) {
-  let minMove = name.length - 1;
+// 구명보트
+function solution(people, limit) {
   let answer = 0;
+  let sortedPeople = people.sort((a, b) => b - a);
 
-  for (let i = 0; i < name.length; i++) {
-    answer += Math.min(
-      name[i].charCodeAt() - "A".charCodeAt(),
-      "Z".charCodeAt() - name[i].charCodeAt() + 1
-    );
-
-    let idx = i + 1;
-    while (idx < name.length && name[idx] === "A") idx++;
-
-    minMove = Math.min(
-      minMove,
-      2 * i + (name.length - idx),
-      i + 2 * (name.length - idx)
-    );
+  for (let i = 0, j = people.length - 1; i <= j; i++) {
+    if (sortedPeople[i] + sortedPeople[j] <= limit) {
+      j--;
+    }
+    answer++;
   }
 
-  return answer + minMove;
+  return answer;
 }
 
-console.log(solution("JEROEN"));
-console.log(solution("JAN"));
+console.log(solution([70, 50, 80, 50], 100));
+console.log(solution([70, 80, 50], 100));
