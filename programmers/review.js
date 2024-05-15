41bbab06c4110c5fd457e74fbcb2be7a6958cc33
seoAreum <1971236@hansung.ac.kr>
2024-05-16 02:02:29 +0900

    review: 05/15

--- programmers/review.js
+++ programmers/review.js
@@ -1,19 +1,35 @@
-// 피로도
-function solution(k, dungeons) {
-  var answer = -1;
-  const visited = Array(dungeons.length).fill(0);
+// 전력망을 둘로 나누기
+function solution(n, wires) {
+  let answer = Number.MAX_VALUE;
+  let cnt = 0;
 
-  dfs(k, 0);
-
-  function dfs(k, cnt) {
-    for (let i = 0; i < dungeons.length; i++) {
-      if (k >= dungeons[i][0] && !visited[i]) {
-        visited[i] = true;
-        dfs(k - dungeons[i][1], cnt + 1);
-        visited[i] = false;
-      }
-    }
-    answer = Math.max(answer, cnt);
+  while (cnt !== wires.length) {
+    const shiftedNode = wires.shift();
+    answer = Math.min(
+      answer,
+      Math.abs(bfs(shiftedNode[0]) - bfs(shiftedNode[1]))
+    );
+    wires.push(shiftedNode);
+    cnt++;
   }
+
+  function bfs(node) {
+    const visited = [];
+    const needVisited = [node];
+
+    while (needVisited.length) {
+      const curNode = needVisited.shift();
+      wires.forEach((wire) => {
+        if (wire.includes(curNode)) {
+          const other = curNode === wire[0] ? wire[1] : wire[0];
+          if (!visited.includes(other)) needVisited.push(other);
+        }
+      });
+      visited.push(curNode);
+    }
+
+    return visited.length;
+  }
+
   return answer;
 }
