ed6fb7fa1afb23a8348b8db1242d5a5fc9832194
seoAreum <1971236@hansung.ac.kr>
2024-05-16 02:26:01 +0900

    review: 05/16

--- programmers/review.js
+++ programmers/review.js
@@ -1,35 +1,23 @@
-// 전력망을 둘로 나누기
-function solution(n, wires) {
-  let answer = Number.MAX_VALUE;
-  let cnt = 0;
-
-  while (cnt !== wires.length) {
-    const shiftedNode = wires.shift();
-    answer = Math.min(
-      answer,
-      Math.abs(bfs(shiftedNode[0]) - bfs(shiftedNode[1]))
-    );
-    wires.push(shiftedNode);
-    cnt++;
+// 모음 사전
+function solution(word) {
+  var answer = 0;
+  const str = "";
+  const result = [];
+  for (let i = 1; i <= 5; i++) {
+    dfs(str, i, result);
   }
 
-  function bfs(node) {
-    const visited = [];
-    const needVisited = [node];
-
-    while (needVisited.length) {
-      const curNode = needVisited.shift();
-      wires.forEach((wire) => {
-        if (wire.includes(curNode)) {
-          const other = curNode === wire[0] ? wire[1] : wire[0];
-          if (!visited.includes(other)) needVisited.push(other);
-        }
-      });
-      visited.push(curNode);
+  function dfs(word, length, result) {
+    const vowels = [..."AEIOU"];
+    if (word.length === length) {
+      result.push(word);
+      return;
     }
 
-    return visited.length;
+    vowels.forEach((vowel) => {
+      dfs(word + vowel, length, result);
+    });
   }
 
-  return answer;
+  return result.sort().indexOf(word) + 1;
 }
