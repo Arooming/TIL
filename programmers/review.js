405a1503bc504a19906ab87d642ada10cd200f2b
seoAreum <1971236@hansung.ac.kr>
2024-05-30 23:08:19 +0900

    review: 05/30

--- programmers/review.js
+++ programmers/review.js
@@ -1,38 +1,18 @@
-// 게임 맵 최단거리
-function solution(maps) {
-  const N = maps.length;
-  const M = maps[0].length;
-  const visited = Array(N)
-    .fill(0)
-    .map(() => Array(M).fill(0));
-  const queue = [];
-  let dist = maps;
-  const dx = [0, 0, -1, 1];
-  const dy = [-1, 1, 0, 0];
+// 타겟 넘버
+function solution(numbers, target) {
+  var answer = 0;
+  dfs(0, 0);
 
-  visited[0][0] = 1;
-  queue.push([0, 0]);
-
-  while (queue.length) {
-    const [x, y] = queue.shift();
-
-    for (let i = 0; i < 4; i++) {
-      const nx = x + dx[i];
-      const ny = y + dy[i];
-
-      if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
-        continue;
+  function dfs(idx, sum) {
+    if (idx === numbers.length) {
+      if (sum === target) {
+        answer++;
       }
-      if (maps[nx][ny] === 0) {
-        continue;
-      }
-      if (!visited[nx][ny]) {
-        queue.push([nx, ny]);
-        visited[nx][ny] = 1;
-        dist[nx][ny] = dist[x][y] + 1;
-      }
+      return;
     }
-  }
 
-  return dist[N - 1][M - 1] === 1 ? -1 : dist[N - 1][M - 1];
+    dfs(idx + 1, sum + numbers[idx]);
+    dfs(idx + 1, sum - numbers[idx]);
+  }
+  return answer;
 }
