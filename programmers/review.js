4c1c170866cb7501a5260a9f0042afd306b88b5d
seoAreum <1971236@hansung.ac.kr>
2024-06-05 16:18:52 +0900

    review: 06/05

--- programmers/review.js
+++ programmers/review.js
@@ -1,17 +1,25 @@
-// 올바른 괄호
-// 예외 1: ')'가 먼저 들어온 경우
-// 예외 2: 스택을 다 돌았는데 스택에 문자열이 남아있는 경우
-function solution(s) {
-  const stack = [];
+// 프로세스
+function solution(priorities, location) {
+  var cnt = 0;
+  const obj = priorities.map((prio, idx) => {
+    return { prio, idx };
+  });
 
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === "(") {
-      stack.push("(");
+  while (obj.length) {
+    const MAX = Math.max(...priorities);
+
+    const process = obj.shift();
+    const prio = priorities.shift();
+    if (process.prio < MAX) {
+      obj.push(process);
+      priorities.push(prio);
     } else {
-      if (!stack.length) return false;
-      stack.pop();
+      cnt++;
+      if (process.idx === location) {
+        return cnt;
+      }
     }
   }
 
-  return stack.length === 0;
+  return cnt;
 }
