5cf8c8491572fcc5a75c2b36374d75d93a8571a3
seoAreum <1971236@hansung.ac.kr>
2024-06-06 10:54:38 +0900

    review: 06/06

--- programmers/review.js
+++ programmers/review.js
@@ -1,8 +1,20 @@
-// 가장 큰 수
-function solution(numbers) {
-  var answer = numbers
-    .map((num) => num.toString())
-    .sort((a, b) => b + a - (a + b))
-    .join("");
-  return answer[0] === "0" ? "0" : answer;
+// 다리를 지나는 트럭
+function solution(bridge_length, weight, truck_weights) {
+  var answer = 0;
+  const bridge = Array(bridge_length).fill(0);
+
+  while (bridge.length) {
+    bridge.shift();
+    if (truck_weights.length) {
+      const sum = bridge.reduce((a, b) => a + b, 0);
+      if (sum + truck_weights[0] <= weight) {
+        const go = truck_weights.shift();
+        bridge.push(go);
+      } else {
+        bridge.push(0);
+      }
+    }
+    answer++;
+  }
+  return answer;
 }
