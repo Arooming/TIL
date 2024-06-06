94719704c4c2e5576ea89e8a893eea316ab8b72c
seoAreum <1971236@hansung.ac.kr>
2024-06-06 11:05:40 +0900

    review: 06/06

--- programmers/review.js
+++ programmers/review.js
@@ -1,16 +1,23 @@
-// 주식 가격
-function solution(prices) {
-  var answer = [];
-  
-  for (let i = 0; i < prices.length; i++) {
-      let cnt = 0;
-      for(let j = i + 1; j < prices.length; j ++) {
-          cnt ++;
-          if(prices[i] > prices[j]) {
-              break;
-          }
-      }
-      answer.push(cnt);
+// 모음 사전
+function solution(word) {
+  let str = "";
+  const result = [];
+
+  function dfs(str, idx, result) {
+    const vowels = [..."AEIOU"];
+    if (str.length === idx) {
+      result.push(str);
+      return;
+    }
+
+    vowels.forEach((vowel) => {
+      dfs(str + vowel, idx, result);
+    });
   }
-  return answer;
+
+  for (let i = 1; i <= 5; i++) {
+    dfs(str, i, result);
+  }
+
+  return result.sort().indexOf(word) + 1;
 }
