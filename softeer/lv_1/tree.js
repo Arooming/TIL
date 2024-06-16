98cede37e0997d4e35bac59734c61522d4b04f88
seoAreum <1971236@hansung.ac.kr>
2024-06-16 22:42:00 +0900

    update: 06/16

---
+++ softeer/lv_1/tree.js
@@ -0,0 +1,24 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let arr = [];
+
+rl.on("line", (input) => {
+  arr = input.split(" ").map((num) => +num);
+});
+
+rl.on("close", () => {
+  const sorted_arr = arr.sort((a, b) => b - a);
+  const last_idx = sorted_arr.length - 1;
+  const front_value = sorted_arr[0] * sorted_arr[1];
+  const back_value = sorted_arr[last_idx] * sorted_arr[last_idx - 1];
+
+  const answer = front_value > back_value ? front_value : back_value;
+
+  console.log(answer);
+
+  process.exit();
+});
