d7224e08b84bb491efd5e235e01aa45e18351783
seoAreum <1971236@hansung.ac.kr>
2024-06-28 21:04:38 +0900

    update: 06/28

--- softeer/lv_2/bfs.js
+++ softeer/lv_2/bfs.js
@@ -1,34 +1,38 @@
-// 1이 장애물 (갈 수 없음), 0이 도로 (갈 수 있음)
-// 첫 번째 줄 출력: 장애물 덩어리의 수
-// 이후 출력: 장애물 자체의 수 오름차순 정렬
+// 1인 곳으로만 지나갈 수 있음
+// 상하좌우 탐색
+// 장애물 블록 수 출력, 각 블록에 속하는 장애물의 수를 오름차순으로 정렬
+
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-let arr = [];
+let input = "";
 
-rl.on("line", (input) => {
-  const map = input.split("").map((v) => +v);
-  arr = [...arr, map];
+rl.on("line", (line) => {
+  input += line + "\n";
 });
 
 rl.on("close", () => {
-  const N = +arr[0][0];
-  let visited = Array(N)
-    .fill(0)
-    .map(() => Array(N).fill(0));
-  const queue = [];
+  const inputArr = input
+    .trim()
+    .split("\n")
+    .map((line) => line.split(" "));
 
-  let maps = arr.slice(1);
-  const answer = [];
+  const N = +inputArr[0][0];
+  const arr = inputArr.slice(1).map((v) => String(v).split("").map(Number));
+  const visited = new Array(N).fill(0).map(() => new Array(N).fill(0));
+
+  const result = [];
 
   const dx = [0, 0, -1, 1];
   const dy = [-1, 1, 0, 0];
 
   function bfs(x, y) {
+    const queue = [];
     let cnt = 1;
+
     queue.push([x, y]);
     visited[x][y] = 1;
 
@@ -36,33 +40,38 @@ rl.on("close", () => {
       const [x, y] = queue.shift();
 
       for (let i = 0; i < 4; i++) {
-        const nx = dx[i] + x;
-        const ny = dy[i] + y;
+        const nx = x + dx[i];
+        const ny = y + dy[i];
 
-        if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-          if (maps[nx][ny] === 1 && !visited[nx][ny]) {
-            queue.push([nx, ny]);
-            visited[nx][ny] = 1;
-            cnt += 1;
-          }
+        if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+          continue;
+        }
+
+        if (arr[nx][ny] === 0) {
+          continue;
+        }
+
+        if (!visited[nx][ny]) {
+          queue.push([nx, ny]);
+          visited[nx][ny] = 1;
+          cnt += 1;
         }
       }
-    } 
-    answer.push(cnt);
+    }
+    result.push(cnt);
   }
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
-      if (maps[i][j] === 1 && !visited[i][j]) {
+      if (arr[i][j] === 1 && !visited[i][j]) {
         bfs(i, j);
       }
     }
   }
-  answer.sort();
+  result.sort((a, b) => a - b);
+  console.log(result.length);
 
-  console.log(answer.length);
-  for (let i = 0; i < answer.length; i++) {
-    console.log(answer[i]);
-  }
+  result.map((v) => console.log(v));
+
   process.exit();
 });
