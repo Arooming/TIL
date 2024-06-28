5587e7968b2d41baf0ae18e8ecc6d4f94c424184
seoAreum <1971236@hansung.ac.kr>
2024-06-28 23:53:20 +0900

    update: 06/28

--- softeer/lv_2/meeting.js
+++ softeer/lv_2/meeting.js
@@ -23,79 +23,56 @@ rl.on("close", () => {
     .split("\n")
     .map((line) => line.split(" "));
 
-  const N = +inputArr[0][0];
-  const M = +inputArr[0][1];
+  const [N, M] = inputArr[0].map(Number);
   const roomName = inputArr
-    .slice(1, N + 1)
-    .map((name) => name.join(""))
+    .slice(1, 1 + N)
+    .map((v) => v.join(""))
     .sort();
-  const reservation = inputArr.slice(N + 1);
+  const reservation = inputArr.slice(1 + N);
 
-  // 예약된 시간 자체를 저장한 배열
-  const timeArr = [];
-
-  // 전체 예약 가능/ 불가능 시간 정보가 저장된 배열
-  const totalTimetable = [];
-
+  const timetable = [];
   const result = [];
 
-  // 예약 불가능 시간 타임 테이블 생성
   for (let i = 0; i < N; i++) {
-    const arr = [];
-
+    let time = new Array(9).fill(0);
     for (let j = 0; j < M; j++) {
-      const room = reservation[j][0];
-      const start = +reservation[j][1];
-      const end = +reservation[j][2];
+      const start = +reservation[j][1] - 9;
+      const end = +reservation[j][2] - 9;
 
-      if (room === roomName[i]) {
-        arr.push([start, end]);
+      if (reservation[j][0] === roomName[i]) {
+        for (let k = start; k < end; k++) {
+          time[k] = 1;
+        }
       }
     }
-    arr.sort((a, b) => a[0] - b[0]);
-    timeArr.push(arr);
-  }
-
-  // 전체 예약 가능/ 불가능 시간 정보 저장
-  for (let i = 0; i < N; i++) {
-    const timetable = new Array(9).fill(0);
-    for (let j = 0; j < timeArr[i].length; j++) {
-      const start = timeArr[i][j][0] - 9;
-      const end = timeArr[i][j][1] - 9;
-
-      for (k = start; k < end; k++) {
-        timetable[k] = 1;
-      }
-    }
-    totalTimetable.push(timetable);
+    timetable.push(time);
   }
 
   for (let i = 0; i < N; i++) {
     const arr = [];
     for (let j = 0; j < 9; j++) {
-      if (totalTimetable[i][j] === 0) {
-        if (j === 0 || totalTimetable[i][j - 1] === 1) {
+      if (timetable[i][j] === 0) {
+        if (j === 0 || timetable[i][j - 1] === 1) {
           arr.push(j + 9);
         }
+        if (j === 8) {
+          arr.push(18);
+        }
       } else {
-        if (j > 0 && totalTimetable[i][j - 1] === 0) {
+        if (timetable[i][j - 1] === 0) {
           arr.push(j + 9);
         }
       }
     }
-    if (totalTimetable[i][8] === 0) {
-      arr.push(18);
-    }
     result.push(arr);
   }
 
-  // 출력
   for (let i = 0; i < N; i++) {
     console.log(`Room ${roomName[i]}:`);
-    if (result[i].length === 0) {
-      console.log("Not available");
-    } else {
+    if (result[i].length) {
       console.log(`${result[i].length / 2} available:`);
+    } else {
+      console.log("Not available");
     }
 
     for (let j = 0; j < result[i].length - 1; j += 2) {
@@ -104,6 +81,7 @@ rl.on("close", () => {
 
       console.log(`${start}-${end}`);
     }
+
     if (i !== N - 1) {
       console.log("-----");
     }
